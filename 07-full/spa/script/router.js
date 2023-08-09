43864862b81a83e557633e8fb87b1dfcbe37bdcb
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-08-09 16:28:17 +0200

    Cours api - spa

---
+++ 07-full/spa/script/router.js
@@ -0,0 +1,64 @@
+"use strict";
+import routes from "./routes.js";
+
+const main = document.querySelector('main');
+
+/**
+ * Affiche le fichier HTML correspondant à la route donnée en paramètre.
+ * Puis lance les scripts correspondants.
+ * @param {string} uri 
+ */
+export default async function router(uri)
+{
+    window.history.pushState({}, "", uri);
+    main.classList.remove("show");
+
+    const path = window.location.pathname;
+    const route = "/view/"+ (routes[path]?.html||"404.html");
+
+    const response = await fetch(route);
+    if(!response.ok)return;
+
+    const data = await response.text();
+    main.innerHTML = data;
+
+    // TODO: gérer les imports JS
+
+    getLinks();
+    main.classList.add("show");
+}
+/**
+ * Selectionne tous les liens de la page et lance la fonction de paramétrage des liens.
+ * @param {HTMLElement} parent 
+ */
+export function getLinks(parent=document)
+{
+    const links = parent.querySelectorAll("a");
+    links.forEach(setLink);
+}
+/**
+ * Paramètre le lien donnée en argument.
+ * @param {HTMLAnchorElement} a 
+ */
+function setLink(a)
+{
+    a.onclick = goToHref;
+    const logged = sessionStorage.getItem("logged");
+    if(a.classList.contains("logged"))
+    {
+        a.parentElement.style.display = logged?"block":"none";
+    }
+    else if(a.classList.contains("logout"))
+    {
+        a.parentElement.style.display = logged?"none":"block";
+    }
+}
+/**
+ * Active la fonction de routing lors du clique sur un lien.
+ * @param {MouseEvent} e 
+ */
+function goToHref(e)
+{
+    e.preventDefault();
+    router(this.href);
+}
